e50211d3068fc2a35ebf91fadad0af25e608b084
Ed <edsagoefvr@gmail.com>
2022-11-29 23:30:28 +0000

    Pylance correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,6 +4,7 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 if number < 0:
     last_digit = -last_digit
+
 if last_digit == 0:
 	print(f"Last digit of {number:d} is {last_digit:d} and is 0")
 elif last_digit > 5:
