e0a35b1844630e7dfcd0dc8c7a5fb45d27c4c533
Ed <edsagoefvr@gmail.com>
2022-11-30 00:23:58 +0000

    Corrections

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,10 +3,8 @@ import random
 number = random.randint(-10, 10)
 
 if number > 0:
-	status = "positive"
+    print(f"{number:d} is positive")
 elif number < 0:
-	status = "negative"
+    print(f"{number:d} is negative")
 else:
-	status = "zero"
-
-print(f"{number:d} is {status}")
+    print(f"{number:d} is zero")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    if c != ord('e') and c != ord('q'):
+for c in range(97, 123):
+    if c != 101 and c != 113:
         print("{:c}".format(c), end="")
-
