3aed1c08bc88a594e0593623815f0adba8fa1719
Ed Sagoe <edsagoefvr@gmail.com>
2024-01-25 05:37:32 +0000

    Add documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 class Square:
+    """Square class defines a square
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new instance of the Square class.
+    
+    """
+    
     def __init__(self, size):
+        """Initializes a new insyance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+
+        """
         self.__size = size
