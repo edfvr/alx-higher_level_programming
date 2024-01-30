7629e345a39316329084d6e23ac7f84bee425c3a
Ed Sagoe <edsagoefvr@gmail.com>
2024-01-30 00:42:53 +0000

    Write a class Rectangle that defines a rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        rect_2
+        return rect_2
 
     @property
     def width(self):
