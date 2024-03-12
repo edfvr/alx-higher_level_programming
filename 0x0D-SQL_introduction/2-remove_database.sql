a2ae2c8e87b5c8edbb34063d65061bba84374bcd
Ed Sagoe <edsagoefvr@gmail.com>
2024-03-12 11:33:43 +0000

    Write a script that lists all the tables of a database in your MySQL server.

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,3 @@
---A script that deletes the database hbtn_0c_0 in your MySQL server.--
+-- Deletes the database hbtn_0c_0 in your MySQL server.
+-- If the database hbtn_0c_0 already exists, your script should not fail
 DROP DATABASE IF EXISTS hbtn_0c_0;
